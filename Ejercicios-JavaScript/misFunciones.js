c7ee755aa5ce87c87b9bffbff1fa29ea1fdc3b3d
juan-ucc <villarrealjm2@gmail.com>
2020-05-27 17:59:08 -0300

    ejercicio radianes grados

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -35,5 +35,25 @@ function conversor(id, valor) {
 
 }
 
+/**
+ * Comversion de unidades (grados a radianes)
+ * @method conRad
+ * @param {string} id - el id de los inputs de grado o radian
+ * @return
+ */
 
+function conRad(id) {
 
+    var grad, rad;
+    if (id=="grados") {
+         grad = document.getElementById("grados").value;
+         rad = (grad*Math.PI)/180;
+    } else if (id=="radianes") {
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI;
+    }
+
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+
+}
